de581aec56874a7d7930ddfdf9089cc33bb628c0
grindelwaldoff <vsevolodrybnik@gmail.com>
2022-11-09 01:26:09 +0300

    sfsdf

--- ozon/index.py
+++ ozon/index.py
@@ -1,6 +1,10 @@
 import os
+import json
 import logging
+import time
+from datetime import datetime as dt
 
+import requests
 from dotenv import load_dotenv
 
 
@@ -10,7 +14,33 @@ BASE_DIR = os.path.dirname(__file__)
 
 TOKEN = os.getenv('TOKEN')
 CLIENT_ID = os.getenv('CLIENT_ID')
-KEY_NAME = os.getenv('KEY_NAME')
+HEADERS = {
+    'Client-Id': CLIENT_ID,
+    'Api-Key': TOKEN,
+    'Content-Type': 'application/json'
+}
+
+BASE_URL_OZON = 'https://api-seller.ozon.ru/v3/posting/fbs/list'
+
+
+def get_list_request_ozon(current_date: str) -> dict:
+    params = {
+        "dir": "ASC",
+        "filter": {
+            "cutoff_from": "2021-08-24T14:15:22Z",
+            "cutoff_to": current_date,
+            "delivery_method_id": [],
+            "provider_id": [],
+            "status": "awaiting_packaging",
+            "warehouse_id": []
+        },
+        "limit": 100,
+        "offset": 0
+    }
+    response = requests.post(url=BASE_URL_OZON, headers=HEADERS, json=params)
+    res = json.loads(response._content)
+    print(res)
+    return res
 
 
 def main():
@@ -25,8 +55,18 @@ def main():
             'info:%(message)s \n')
     )
 
+    current_date = str(dt.utcnow().isoformat()+'Z')
+
     while True:
-        pass
+        check_time = dt.now().hour
+        print(check_time)
+        try:
+            ozon_list = get_list_request_ozon(current_date)
+            break
+        except Exception as error:
+            logging.error(error)
+        else:
+            break
 
 
 if __name__ == '__main__':
